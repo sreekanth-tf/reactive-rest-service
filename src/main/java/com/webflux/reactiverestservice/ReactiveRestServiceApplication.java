556a03f8d96a85e88e4d4d298fd97fc66bbee593
sreekanth-tf <sreekanth.nair@thoughtfocus.com>
2021-08-27 12:14:20 +0530

    Initial commit

---
+++ src/main/java/com/webflux/reactiverestservice/ReactiveRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.webflux.reactiverestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveRestServiceApplication.class, args);
+	}
+
+}
